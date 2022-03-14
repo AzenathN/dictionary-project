26844ba2681c7d068d1b8bfed45b3275292f1343
AzenathN <81848056+AzenathN@users.noreply.github.com>
2022-03-14 00:10:37 -0700

    Starting a new app with React

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="" className="App-logo" alt="logo" />
+        <p>
+          This will be a dictionary app <code>src/App.js</code> and save to
+          reload.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
